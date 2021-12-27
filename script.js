ac1df1a61350b9af669a010e848cfad038aeb3c9
ilonaGold <songa17@inbox.lv>
2021-12-27 13:42:29 +0200

    Added base javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+const input = document.querySelector("input"),
+    add = document.querySelector("#add");
+    todoList = document.querySelector(".todo-list"),
+    clear = document.querySelector("#clear");
+
+
+const addTask = (e) => {
+    e.preventDefault(e);
+    const newLi = document.createElement("li");
+    const delBtn = document.createElement("button");
+
+    delBtn.innerHTML = "<i class='fas fa-trash-alt'></i>";
+
+    if (input.value !== "") {
+        newLi.textContent = input.value;
+        newLi.appendChild(delBtn);
+        todoList.appendChild(newLi);
+        input.value = "";
+    } else {
+        alert("Please enter a task");
+    }
+
+    delBtn.addEventListener("click", function() {
+        const del = confirm("Are you sure to delete this task?")
+        if (del === true) {
+            const parent = this.parentNode;
+            parent.remove();
+        }
+    });
+};
+
+add.addEventListener("click", addTask);
+
+clear.addEventListener("click", () => {
+    todoList.innerHTML = "";
+});
+
